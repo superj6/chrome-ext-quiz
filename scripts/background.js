b7b746433f16b4a507a7491ad7b0771acff1a7f1
rythm <jgon@mit.edu>
2024-03-19 00:10:45 -0500

    save quizinterval to localstorage, make quiz alarm use random time in saved interval

--- scripts/background.js
+++ scripts/background.js
@@ -1,6 +1,30 @@
-function setQuizTimer(){
+function getRandomInt(min, max){
+  return min + Math.floor(Math.random() * (max - min + 1));
+}
+
+function setQuizInterval(interval){
+  chrome.storage.local.set({
+    quizInterval: interval
+  });
+}
+
+async function getQuizInterval(){
+  let interval = await chrome.storage.local.get('quizInterval').quizInterval;
+  if(typeof interval === 'undefined'){
+    interval = {min: 1, max: 2};
+    setQuizInterval(interval);
+  }
+  return interval;
+}
+
+async function setQuizTimer(){
+  let interval = await getQuizInterval();
+  let delay = getRandomInt(interval.min, interval.max);
+
+  console.log(`setQuizTimer delay: ${delay}`);
+
   chrome.alarms.create('popquiz', { 
-    delayInMinutes: 1
+    delayInMinutes: delay
   });
 }
 
